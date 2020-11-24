1838f853763bbc4136fcfb5c336a593df55188f2
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-11-24 20:55:40 +0800

    [PosMachinePractice][Jackie] enhancement:added PDCA and Context map

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -5,7 +5,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class PosMachine {
-	
+// P: 2 hours
+// D: 1.5 hours
+// C: Used helpful data structures like HashMap and List reduced a lot of development time, but as I am not familiar with Java data structure, googling takes a while
+// A: Keep trying to use these data structures in future development work. Study more on java data structures
     public String printReceipt(List<String> barcodes) {
     	List<ItemInfo> itemInfos = ItemDataLoader.loadAllItemInfos();
     	HashMap<String,Integer> itemCountMap = getItemCountMap(barcodes);
@@ -18,6 +21,10 @@ public class PosMachine {
         return renderReceipt(allItemDetails,total);
     }
     
+ // P: 20 mins
+ // D: 5 mins
+ // C: At first it seemed complicated as I could only think of using loop to doing counting, but I searched for better methods online and have learnt to use HashMap to do it
+ // A: Keep this in mind and try to adopt HashMap in similar tasks in the future. Study more on HashMap
     private HashMap<String,Integer> getItemCountMap(List<String> barcodes) {
     	HashMap<String, Integer> itemCountMap = new HashMap<String, Integer>();
     	for(String item:barcodes) {
@@ -27,6 +34,11 @@ public class PosMachine {
     	}
     	return itemCountMap;
     }
+    
+ // P: 20 mins
+ // D: 10 mins
+ // C: At first I planned to generate ItemDetail one by one, but it turns out I could iterate through the list and get the ItemDetail of all the items all at once, so it saved some time
+ // A: Can use this experience to do better planning during context map stage
     
     private List<ItemDetail> getItemDetails(List<ItemInfo> itemInfos,HashMap<String,Integer> itemCountMap) {
     	List<ItemDetail> allItemDetails = new ArrayList<>();
@@ -40,10 +52,19 @@ public class PosMachine {
     	return allItemDetails;
     }
     
+ // P: 1 mins
+ // D: 1 mins
+ // C: The time used is as expected.
+ // A: Keep making achievable planning like this.
+    
     private int calculateSubtotal(int price, int quantity) {
     	return price * quantity;
     }
-    
+
+// P: 2 mins
+// D: 2 mins
+// C: The time used is as expected.
+// A: Keep making achievable planning like this.
     private int calculateTotal(List<ItemDetail> allItemDetails) {
     	int total = 0;
     	for(ItemDetail item:allItemDetails) {
@@ -51,6 +72,12 @@ public class PosMachine {
     	}
     	return total;
     }
+    
+// P: 2 mins
+// D: 2 mins
+// C: The time used is as expected.
+// A: Keep making achievable planning like this.
+       
     
     private String renderReceipt(List<ItemDetail> allItemDetails, int total) {
     	String receipt =  "***<store earning no money>Receipt***\n";
